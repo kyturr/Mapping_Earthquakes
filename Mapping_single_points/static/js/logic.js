0d584a76ad5a12496ba34c244e6c5e3be682445d
unknown <kyturr@gmail.com>
2022-08-24 23:27:57 -0700

    challenge

--- Mapping_single_points/static/js/logic.js
+++ Mapping_single_points/static/js/logic.js
@@ -1,8 +1,62 @@
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 16);
+let map = L.map('mapid').setView([39.8283, -98.5795], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+        // //  Add a marker to the map for Los Angeles, California.
+        // let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+
+
+// Loop through the cities array and create one marker for each city.
+// An array containing each city's location, state, and population.
+let cities = [{
+    location: [40.7128, -74.0059],
+    city: "New York City",
+    state: "NY",
+    population: 8398748
+  },
+  {
+    location: [41.8781, -87.6298],
+    city: "Chicago",
+    state: "IL",
+    population: 2705994
+  },
+  {
+    location: [29.7604, -95.3698],
+    city: "Houston",
+    state: "TX",
+    population: 2325502
+  },
+  {
+    location: [34.0522, -118.2437],
+    city: "Los Angeles",
+    state: "CA",
+    population: 3990456
+  },
+  {
+    location: [33.4484, -112.0740],
+    city: "Phoenix",
+    state: "AZ",
+    population: 1660272
+  }
+  ];
+
+// Get data from cities.js
+let cityData = cities;
+cityData.forEach(function(city) {
+    console.log(city)
+    // L.marker(city.location).addTo(map)
+    L.circleMarker(city.location,{
+        radius: city.population/200000,
+        color: "orange",
+        fillcolor:"orange",
+        fillOpacity: 0.5,
+        weight:4
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+   });
+
+// Get data from cities.js
 // Add circle to map
 L.circle([34.0522, -118.2437], {
     fillOpacity: 0.5,
